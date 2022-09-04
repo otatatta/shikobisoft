a333fa2f79be657c2e136cdc71545aaafd4df37a
otatatataspin <yuriabeizhong@gmail.com>
2022-09-04 20:46:00 +0900

    blogs

---
+++ pages/component/Header.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import shikoIcon from "../static/shikoIcon.png"
+
+function Header() {
+
+  return (
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/" style={{ width: "40%" }}>
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+            <Image
+              alt="Shikoshiko"
+              src={shikoIcon}
+              objectFit="contain"
+              width={100}
+              height={100}
+            />
+          </div>
+        </Link>
+        <span >SHIKOSHIKOSOFT OFFICAL WEBSITE</span>
+        <span >twiter</span>
+      </div>
+      <div className="flex-1 items-center justify-center space-x-8 md:flex">
+        <Link href="./blog">
+          <a className="headerLink">Blog</a>
+        </Link>
+        <a className="headerLink">Staff</a>
+        <a className="headerLink">Product</a>
+        <a className="headerLink">Contact</a>
+      </div>
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+        <Link href="/checkout">
+          <div className="relative cursor-pointer">
+          </div>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,13 @@
-import Link from "next/link";
-// import Image from 'next/image'
-import { client } from "../libs/client";
 import styles from "../styles/Home.module.css";
+import Header from "./component/Header"
 
-//SSG
-export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blogs" });
-  console.log(data);
-  return {
-    props: {
-      blog: data.contents,
-    },
-  };
-};
 
 export default function Home({ blog }) {
   return (
     <div className={styles.container}>
-      {blog.map((blog) => (
-        <li key={blog.id}>
-          <Link href={`blog/${blog.id}`}>
-            {/* <a href="">
-              {blog.eyecatch ? (
-                <Image
-                  src={blog.eyecatch.url}
-                  width={blog.eyecatch.width}
-                  height={blog.eyecatch.height}
-                />
-              ) : (
-                <Image src="/static/shikoIcon.png" alt="No Image" />
-              )}
-            </a> */}
-            <a href="">{blog.title}</a>
-          </Link>
-        </li>
-      ))}
+      <Header />
+
+      しこしこそふと（仮）
     </div>
   );
 }
