7e557d58074d81bb98ce34c083bc8a36484970f1
otatatataspin <yuriabeizhong@gmail.com>
2022-11-08 03:01:57 +0900

    キャラページサンプル

--- pages/mayukara/character/index.jsx
+++ pages/mayukara/character/index.jsx
@@ -1,92 +1,148 @@
 import OtherHeader from "../components/OtherHeader"
 import Footer from "../../component/Footer"
-import { Modal } from 'antd';
-import React, { useState } from 'react';
+import { Col, Row, Modal, Segmented } from 'antd';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image'
 import Background from "../../static/hp1_bg.png"
-import iconMamoru from "../../static/icons/iconmmr.png"
-import mamoru1 from "../../static/stands/HP1mmr.png"
-import mamoru2 from "../../static/stands/HP1mmr2.png"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
-// import iconMamoru from "../../static/stands/"
+import CharaWrapper from "./charaInfos"
 
-const mamoru = {
-    names: "犬無 守琉",
-    name_kana: "Inunashi Mamoru",
-    txt: (
-        <div>
-            <p>
-                『 ――全部が全部、気色が悪いよな…… 』<br />
-                本作の主人公である十九歳の青年。<br />
-                舞台である繭ノ葉町を、実母と共に幼い頃に外へと連れ出た。しかし母の消息不明を機に、十四年振りに町へと帰省することとなる。<br />
-                無愛想気味だが本人なりには人を気遣おうとする故に、困る人などを放っておくことができない気質。<br />
-                繭ノ葉町《御三家》の【神来社家】出自であり、長男ゆえ本来は当主を引き継ぐ立場であるのだが、外の世界で育ったため実家への思い入れなどは何も無い。<br />
-                <br />
-                キャラクター原画: ぎゃばん<br />
-                キャラクター彩色: 麩幸地<br />
-                キャラクターデザイン原案: 麻木ななみ
-            </p>
-        </div>
-    ),
-    icon: <Image
-        src={iconMamoru}
-        alt="Image"
-        width={100}
-        height={100} />,
-    stand_1: <Image
-        src={mamoru1}
-        alt="Image" />,
-    stand_2: <Image
-        src={mamoru2}
-        alt="Image" />,
-}
-
-// export const charaList = [
-//     mamoru
-// ]
 
 export default function MayuChara() {
     const [mamoruOpen, setMamoruOpen] = useState(false);
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height,
+        };
+    }
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    useEffect(() => {
+        const { width, height } = getWindowDimensions();
+
+        setWidth(width);
+        setHeight(height);
+    }, []);
+    useEffect(() => {
+        function handleResize() {
+            const { width, height } = getWindowDimensions();
+            setWidth(width)
+            setHeight(height)
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize)
+    }, [])
+
+    const [charaValue, setCharaValue] = useState();
+
+    const onClickButton = (val) => {
+        setCharaValue({
+            names: val.names,
+            selected_name: val.selected_name,
+            txt: val.txt,
+            icon: val.icon,
+            stand_1: val.stand_1,
+            stand_2: val.stand_2,
+            options: val.options,
+        })
+        setMamoruOpen(true)
+        setValue("1")
+    }
+
+    const [value, setValue] = useState('1');
+
+    console.log(width, height)
+
     return (
         <>
             <OtherHeader />
-            <div style={{ maxWidth: "1280px", marginLeft: "auto", marginRight: "auto" }}>
+            <div style={{ maxWidth: "1280px", marginLeft: "auto", marginRight: "auto", textAlign: "center" }}>
                 <div>
-                    <div style={{ padding: "1rem" }}>
-                        <button type="primary" onClick={() => setMamoruOpen(true)}>
-                            {mamoru?.icon}
-                        </button>
-                        <Modal
-                            title=""
-                            centered
-                            visible={mamoruOpen}
-                            onOk={() => setMamoruOpen(false)}
-                            onCancel={() => setMamoruOpen(false)}
-                            width={"90%"}
-                        >
-                            <Image
-                                alt="Shikoshikoback"
-                                src={Background}
-                                style={{ position: "fixed", zIndex: 4545 }}
-                                width={"100%"}
-                                height={"100%"}
-                                layout={`fill`}
-                                objectFit={`cover`} />
-                            <div style={{ height: "auto" }}></div>
-                            {mamoru.txt}
-                        </Modal>
-                        <div>
-                            {mamoru?.names}
-                        </div>
-                    </div>
-                    <Footer />
+                    <p>キャラクター紹介</p>
                 </div>
+                <div style={{ padding: "1rem" }}>
+                    <Row
+                        gutter={{
+                            xs: 8,
+                            sm: 16,
+                            md: 24,
+                            lg: 32,
+                        }} align="middle"
+                    >
+                        {CharaWrapper.PCList1?.map((list) =>
+                        (
+                            <Col span={3} >
+                                <button type="primary" onClick={() => onClickButton(list)}>
+                                    {list?.icon}
+                                </button>
+                                <div>
+                                    {list?.names}
+                                </div>
+                            </Col>)
+                        )}
+                    </Row>
+                    <Modal
+                        centered
+                        visible={mamoruOpen}
+                        onCancel={() => {
+                            setMamoruOpen(false)
+                            setCharaValue(undefined)
+                            setValue("1")
+                        }}
+                        width={"90%"}
+                        bodyStyle={{ height: `${height * 0.9}px` }}
+                        cancelText="閉じる"
+                        zIndex={14545}
+                        footer={<></>}
+                    >
+                        <Image
+                            alt="Shikoshikoback"
+                            src={Background}
+                            style={{ opacity: 0.33 }}
+                            layout={`fill`}
+                            objectFit={`cover`} />
+
+                        <Row
+                            gutter={{ xs: 8, sm: 16, md: 24, }} align="middle"
+                        >
+                            <Col span={8} >
+                                <div>
+                                    {value === "1" && (
+                                        <Image
+                                            src={charaValue?.stand_1}
+                                            style={{ display: "inline-block" }}
+                                            alt="Image" />
+                                    )}
+                                    {value === "2" && (
+                                        <Image
+                                            src={charaValue?.stand_2}
+                                            style={{ display: "inline-block" }}
+                                            alt="Image" />
+                                    )}
+                                </div>
+                            </Col>
+                            <Col span={16} align="bottom">
+                                <Segmented options={charaValue?.options ?? ["1"]} value={value} onChange={setValue} size={"middle"} />
+                                <div>
+                                    <p style={{
+                                        fontSize: "32px"
+                                    }}>
+                                        {charaValue?.names}
+                                    </p>
+                                    {charaValue?.txt && (
+                                        <div style={{ zIndex: 194545, color: "black" }}>
+                                            {charaValue.txt}
+                                        </div>
+                                    )}
+                                </div>
+                            </Col>
+                        </Row>
+                    </Modal>
+                </div>
+                <Footer />
             </div>
         </>
     );
